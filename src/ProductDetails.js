3cd491291a0ec78f5a4be6397dc3d1a1f69bf16d
bssavinash202 <bssavinash202@gmail.com>
2024-06-25 14:15:31 +0530

    Added some Changes

--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -4,7 +4,7 @@ const ProductDetails = ({Name, Price, Description, image}) =>{
 <div className="product-details">
     <h1>Name of the Product is : {Name}</h1>
     <h2>Price of the {Name} is : {Price} Lakhs</h2>
-    <h2>Here are some key Feature of {Name}:</h2>
+    <h2>Here are some key Feature of the {Name}:</h2>
     <p>{Description}</p>
     <img src="https://www.iamabiker.com/wp-content/uploads/2020/09/Royal-Enfield-Classic-350-BS6-Stealth-Black-HD-wallpapers-9.jpg" alt="Bullet" height = "400"></img>
 </div>
