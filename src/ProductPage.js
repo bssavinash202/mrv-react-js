5690371db2d69ab80e1d4cfc8894e19ca681dc44
bssavinash202 <bssavinash202@gmail.com>
2024-06-25 14:16:56 +0530

    Added Some Changes

--- src/ProductPage.js
+++ src/ProductPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ProductDetails from "./ProductDetails";
 const ProductPage = () =>{
-    const Name = " Royal Enfield Classic 350 Stealth Black";
+    const Name = " Royal Enfield Classic 350 Stealth Black BSVI";
     const Price = 2.25;
     const Description = "The attractive mixture of traditional charm and modern flair found in the Royal Enfield Classic 350 Dark Stealth Black is unmistakable. Thanks to its matte black finish, blacked-out components, and understated chrome accents, it has a unique and fashionable appearance that is certain to catch people's attention.";
     return(
